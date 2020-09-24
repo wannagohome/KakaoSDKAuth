4694eeca9edae7722d84105a8e1a309de229ce78
wannagohome <iwwuf7@gmail.com>
2020-09-24 15:42:49 +0900

    Initial Commit

---
+++ KakaoSDKAuth/KakaoSDKAuth.h
@@ -0,0 +1,19 @@
+//
+//  KakaoSDKAuth.h
+//  KakaoSDKAuth
+//
+//  Created by jinho jang on 2020/09/24.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KakaoSDKAuth.
+FOUNDATION_EXPORT double KakaoSDKAuthVersionNumber;
+
+//! Project version string for KakaoSDKAuth.
+FOUNDATION_EXPORT const unsigned char KakaoSDKAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKAuth/PublicHeader.h>
+
+
